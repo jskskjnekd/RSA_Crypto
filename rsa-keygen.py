ca9c299fbee7dba79c537fcde36770642b723cd9
Harrison Hicks <hhicks13@vols.utk.edu>
2018-11-12 15:47:56 -0500

    finished rsa-keygen

--- rsa-keygen.py
+++ rsa-keygen.py
@@ -1,14 +1,37 @@
 from . import *
 from util import *
+import random
 
-class rsa-keygen:
+class rsa_keygen:
 	def __init__(self, num_bits):
-		self.p = util.generatePrime(num_bits)
-		self.q = util.generatePrime(num_bits)
+		self.p = generatePrime(num_bits)
+		self.q = generatePrime(num_bits)
 		self.n = self.p * self.q
 		self.phi = (self.p - 1)*(self.q - 1)
 	
-	def gen_keypair(self, p , q):
-		if not isPrime(p) and isPrime(q) and (p == q):
-			raise ValueError('issue with prime number generation')
+	def gen_keypair(self):
+
+		p = self.p
+		q  = self.q
+
 		
+
+		e = 65537
+		while not GCD(e,self.phi) == 1:
+			e = random.randrange(1,phi)
+		
+		d = extGCD(e, self.phi)
+		
+		return ((e,self.n),(d, self.n))
+
+if __name__ == '__main__':
+
+	rsa = rsa_keygen(1024)
+	pub,priv = rsa.gen_keypair()
+	
+	for element in pub:
+		print(element)
+
+	for element in priv:
+		print(element)
+	
