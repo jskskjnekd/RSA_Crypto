bdec80a6a35db704e2dd30b7123b8e9eaf2dd832
Harrison Hicks <hhicks13@vols.utk.edu>
2018-11-15 22:07:07 -0500

    added pad

--- RSA.py
+++ RSA.py
@@ -1,4 +1,5 @@
 import util
+import os
 
 """
 :: 11/12/18   11:50 AM
@@ -8,6 +9,22 @@ import util
 class RSACipher:
     def __init__(self):
         pass
+    #expects m to be bytes, N to be integer
+    def pad(m,N):
+        n = int(N // 2)
+        if len(m) > len(n):
+            raise ValueError('message size exceed n bits')
+        else:
+            r_len = int(n//2) - 24
+            pad = b"\0x00\x00"+str(os.urandom(r_len))+b"\x00"
+            padded_m = pad + m
+            print("THIS VALUE SHOULD ALWAYS BE THE SIZE OF n :"+str(len(padded_m)))
+            return padded_m
+
+
+
+
+
 
     def Encryption(N, e, m):
         """
--- test_main.py
+++ test_main.py
@@ -14,40 +14,54 @@ class TestMain(TestCase):
 
     def test_main(self):
         count = 0
-        for round in range(100):
-            # generating keygen            
+        for round in range(3,10):
+        #-------------------------------- BEGIN GENERATING KEY FILES
+            security_param = 256*2
+            n = ' '+str(security_param)
 
+            print("....... ", str(int((security_param//8)/2)) )
 
+            o_cipher_file = 'testData/cipher_output'
+            o_plain_file = 'testData/plain_output'
+
+            privateKey_file = 'testData/privKey'
+            publicKey_file = 'testData/pubKey'
+            keygen_command = 'python3 rsa-keygen.py -p '+publicKey_file+' -s '+privateKey_file+' -n '+n
+
+            os.system(keygen_command)            
+
+	    #-------------------------------- FINISH GENERATING KEYFILES
+            
+            #privateKeyFile = 'testData/privKey1.txt'
+            #publicKeyFile = 'testData/pubKey1.txt'
+            #encryptedFile = originalPlainFile + "_enc"
+            #decryptedFile = originalPlainFile + "_dec"
 
             os.system("rm testData/testPlain*")
             print ("\n"*5)
             count = 1
             numBits = 256
             originalPlainFile = self.generateRandomPlainFile(numBits, count)
-            privateKeyFile = 'testData/privKey1.txt'
-            publicKeyFile = 'testData/pubKey1.txt'
-            encryptedFile = originalPlainFile + "_enc"
-            decryptedFile = originalPlainFile + "_dec"
 
             print('\x1b[1;31;47m' + "\nEncryption.... " + str(round) + '\n' + '\x1b[0m')
-            print(originalPlainFile, privateKeyFile, publicKeyFile, encryptedFile, decryptedFile)
-            encryption_command = "python3 rsa-enc.py " + " -k " + privateKeyFile + "  -i  " + originalPlainFile + "  -o  " + encryptedFile
+            print(originalPlainFile, privateKey_file, publicKey_file, o_cipher_file, o_plain_file)
+            encryption_command = "python3 rsa-enc.py " + " -k " + publicKey_file + "  -i  " + originalPlainFile + "  -o  " + o_cipher_file
             os.system(encryption_command)
             print('\x1b[1;31;47m' + "\nDecryptrion.... " + str(round) + '\n' + '\x1b[0m')
-            decryption_command = "python3 rsa-dec.py " + " -k " + publicKeyFile + "  -i  " + encryptedFile + "  -o  " + decryptedFile
+            decryption_command = "python3 rsa-dec.py " + " -k " + publicKey_file + "  -i  " + o_cipher_file + "  -o  " + o_plain_file
             os.system(decryption_command)
             # -----------------------
             with open(originalPlainFile, 'rb') as originalPlainFileContent:
                 originalPlainFileData = originalPlainFileContent.read()            #-----------------------
-            with open(decryptedFile, 'rb') as decryptedFileContent:
+            with open(o_plain_file, 'rb') as decryptedFileContent:
                 decryptedFileData = decryptedFileContent.read()
             # -----------------------
             try:
-                self.assertEqual(hashlib.sha3_256(originalPlainFileData).hexdigest(), hashlib.sha3_256(decryptedFileData).hexdigest())
+                self.assertEqual(hashlib.sha3_256(originalPlainFileData).hexdigest(), hashlib.sha3_256(o_plain_file).hexdigest())
                 print('\x1b[6;30;42m' + "\nround " +  str(round) + ' Success!\n' + '\x1b[0m')
             except:
                 print('\x1b[3;30;41m' + "\nround " + str(round) + ' Failed!\n' + '\x1b[0m')
-                os.system('cp ' + originalPlainFileData + " _" + originalPlainFileData[10:])
+
 
     def test_intToByte(self):
         t = 361228602529821044081166545515048259738259280980846596864414691243720388090
